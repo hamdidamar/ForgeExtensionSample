52601b8e0bd5a1fc3eff9daaec88127498a73887
Hamdi Damar <hmdmr1@hotmail.com>
2021-02-23 13:16:19 +0300

    Some extension added

--- ForgeExtensionSample/wwwroot/js/ForgeViewer.js
+++ ForgeExtensionSample/wwwroot/js/ForgeViewer.js
@@ -7,7 +7,7 @@ function launchViewer(urn) {
     };
 
     Autodesk.Viewing.Initializer(options, () => {
-        viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: ['Autodesk.DocumentBrowser'] });
+        viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: ['Autodesk.DocumentBrowser', 'MyAwesomeExtension', 'HandleSelectionExtension', 'ModelSummaryExtension'] });
         viewer.start();
         var documentId = 'urn:' + urn;
         Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
