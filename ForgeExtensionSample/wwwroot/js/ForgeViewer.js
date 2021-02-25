016f7582188ff83d434cd3a9a97cf8e696dd229a
Hamdi Damar <hmdmr1@hotmail.com>
2021-02-25 12:42:58 +0300

    transform link added

--- ForgeExtensionSample/wwwroot/js/ForgeViewer.js
+++ ForgeExtensionSample/wwwroot/js/ForgeViewer.js
@@ -7,7 +7,7 @@ function launchViewer(urn) {
     };
 
     Autodesk.Viewing.Initializer(options, () => {
-        viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: ['HandleSelectionExtension', 'ModelSummaryExtension', 'Autodesk.ADN.Viewing.Extension.TransformTool', 'GridExtension']] });
+        viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: ['HandleSelectionExtension', 'ModelSummaryExtension', 'Autodesk.ADN.Viewing.Extension.TransformTool', 'GridExtension','TransformationExtension']] });
 
         viewer.start();
         var documentId = 'urn:' + urn;
